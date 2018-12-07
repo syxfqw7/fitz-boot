9e156fe7d9ca9d84337ea7d379c840a9451a34c3
553583365@qq.com <xuefeiQW9>
2018-12-07 20:45:01 +0800

    start vue

---
+++ src/main/java/cn/bw/fitzboot/controller/ConsoleController.java
@@ -0,0 +1,29 @@
+/**
+ * FileName: ConsoleController
+ * Author:   jack.xue
+ * Date:     2018/12/5 17:27
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * jack.xue           2018/12/5           1.0.0              描述
+ */
+package cn.bw.fitzboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈TODO〉<br> 
+ *
+ * @author jack.xue
+ * @create 2018/12/5
+ * @since 1.0.0
+ */
+@Controller
+public class ConsoleController {
+
+    @RequestMapping("/console")
+    public String console(){
+        return "console";
+    }
+}
---
+++ src/main/resources/static/js/vue/console-leftmenu.js
@@ -0,0 +1,68 @@
+Vue.component('console-leftmenu', {
+    /*props:{
+        asideWidth: String,
+        isCollapse: String
+    },*/
+    props: ['asideWidth','isCollapse'],
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<el-aside style="background-color: rgb(238, 241, 246)" width="{{ asideWidth }}">\n' +
+        '                    <el-menu :default-openeds="[\'2\']" :collapse="true" style="height: 100%" :collapse-transition="false">\n' +
+        '                        <el-submenu index="1">\n' +
+        '                            <template slot="title">\n' +
+        '                                <i class="el-icon-menu"></i>\n' +
+        '                                <span slot="title">导航一</span>\n' +
+        '                            </template>\n' +
+        '                            <el-menu-item-group>\n' +
+        '                                <template slot="title">分组一</template>\n' +
+        '                                <el-menu-item index="1-1">选项1</el-menu-item>\n' +
+        '                                <el-menu-item index="1-2">选项2</el-menu-item>\n' +
+        '                            </el-menu-item-group>\n' +
+        '                            <el-menu-item-group title="分组2">\n' +
+        '                                <el-menu-item index="1-3">选项3</el-menu-item>\n' +
+        '                            </el-menu-item-group>\n' +
+        '                            <el-submenu index="1-4">\n' +
+        '                                <template slot="title">选项4</template>\n' +
+        '                                <el-menu-item index="1-4-1">选项4-1</el-menu-item>\n' +
+        '                            </el-submenu>\n' +
+        '                        </el-submenu>\n' +
+        '                        <el-submenu index="2">\n' +
+        '                            <template slot="title"><i class="el-icon-menu"></i>\n' +
+        '                                <span slot="title">导航二</span>\n' +
+        '                            </template>\n' +
+        '                            <el-menu-item-group>\n' +
+        '                                <template slot="title">分组一</template>\n' +
+        '                                <el-menu-item index="2-1">选项1</el-menu-item>\n' +
+        '                                <el-menu-item index="2-2">选项2</el-menu-item>\n' +
+        '                            </el-menu-item-group>\n' +
+        '                            <el-menu-item-group title="分组2">\n' +
+        '                                <el-menu-item index="2-3">选项3</el-menu-item>\n' +
+        '                            </el-menu-item-group>\n' +
+        '                            <el-submenu index="2-4">\n' +
+        '                                <template slot="title">选项4</template>\n' +
+        '                                <el-menu-item index="2-4-1">选项4-1</el-menu-item>\n' +
+        '                            </el-submenu>\n' +
+        '                        </el-submenu>\n' +
+        '                        <el-submenu index="3">\n' +
+        '                            <template slot="title"><i class="el-icon-setting"></i>\n' +
+        '                                <span slot="title">设置</span>\n' +
+        '                            </template>\n' +
+        '                            <el-menu-item-group>\n' +
+        '                                <template slot="title">分组一</template>\n' +
+        '                                <el-menu-item index="3-1">选项1</el-menu-item>\n' +
+        '                                <el-menu-item index="3-2">选项2</el-menu-item>\n' +
+        '                            </el-menu-item-group>\n' +
+        '                            <el-menu-item-group title="分组2">\n' +
+        '                                <el-menu-item index="3-3">选项3</el-menu-item>\n' +
+        '                            </el-menu-item-group>\n' +
+        '                            <el-submenu index="3-4">\n' +
+        '                                <template slot="title">选项4</template>\n' +
+        '                                <el-menu-item index="3-4-1">选项4-1</el-menu-item>\n' +
+        '                            </el-submenu>\n' +
+        '                        </el-submenu>\n' +
+        '                    </el-menu>\n' +
+        '                </el-aside>'
+});
